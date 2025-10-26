d8b68a03e690beb832c6f627881b8e739938bbc1
Jorge <11655449@alu.murciaeduca.es>
2025-10-26 20:45:18 +0100

    commit 5

--- src/main/java/es/cifpcarlos3/actividad1_7/vo/CatalogoCoches.java
+++ src/main/java/es/cifpcarlos3/actividad1_7/vo/CatalogoCoches.java
@@ -1,4 +1,22 @@
 package es.cifpcarlos3.actividad1_7.vo;
 
+import lombok.Data;
+import tools.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
+import tools.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@JacksonXmlRootElement(localName = "catalogo")
+
 public class CatalogoCoches {
+
+    @JacksonXmlElementWrapper(useWrapping = false)
+    private List<Coches> coche = new ArrayList<>();
+
+    public void add(Coches c) {
+        coche.add(c);
+    }
 }
+
--- src/main/java/es/cifpcarlos3/actividad1_7/vo/Coches.java
+++ src/main/java/es/cifpcarlos3/actividad1_7/vo/Coches.java
@@ -1,4 +1,19 @@
 package es.cifpcarlos3.actividad1_7.vo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class Coches {
+    String marca;
+    String modelo;
+    String color;
+    int anio;
+
+    @Override
+    public String toString() {return "'" + marca + "'" + " '" + modelo + "'" + " '" + color + "'" + anio + "'";}
 }
